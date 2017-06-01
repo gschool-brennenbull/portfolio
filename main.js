7c20c4cf789b92f766764cc80b050eb1dedb5201
Brennen Barret Bull <brennenbull@Brennens-MacBook-Pro.local>
2017-06-01 14:21:43 -0600

    Add all four sides

--- main.js
+++ main.js
@@ -1,15 +1,33 @@
 $(document).ready(()=>{
-  setX();
+  setTransform();
   setWrapper();
 });
 
 let cubeWidth = $('.wrapper').width();
 
-function setX(){
-  $('.back').css('transform','translateX('+cubeWidth/2+'px) rotateY(90deg)');
+function setTransform(){
+  $('.right').css('transform','translateX('+cubeWidth/2+'px) rotateY(90deg)');
   $('.front').css('transform','translateZ('+cubeWidth/2+'px)');
+  $('.left').css('transform','translateX(-'+cubeWidth/2+'px) rotateY(-90deg)');
+  $('.back').css('transform','translatez(-'+cubeWidth/2+'px) rotateX(180deg)')
 }
 
 function setWrapper(){
   $('.wrapper').css('transform','translateZ( -'+cubeWidth+'px)');
 }
+
+$('.menu').on('click', function(){
+  if(!$(this).hasClass('rotateRight')){
+    $(this).removeClass('rotateLeft');
+    $(this).addClass('rotateRight');
+  }else{
+    $(this).removeClass('rotateRight');
+    $(this).addClass('rotateLeft');
+  }
+});
+
+
+
+// $('.menu').on('click', function(){
+//
+// });
