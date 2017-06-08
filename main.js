d8c1130716d8d81d12f9b61e8bb630e79a2f3bb2
Brennen Barret Bull <brennenbull@Brennens-MacBook-Pro.local>
2017-06-08 11:15:07 -0600

    smooth animation for grow

--- main.js
+++ main.js
@@ -22,17 +22,44 @@ function setWrapper(){
   $('.wrapper').css('transform','translateZ( -'+cubeWidth+'px)');
 }
 
-$('.menu').on('click', function(){
-  if(!$(this).hasClass('rotateRight')){
-    $(this).removeClass('rotateLeft');
-    $(this).addClass('rotateRight');
+
+$('.wrapper').on('click', function(ele){
+  var $target = $(ele.target);
+  console.log($target.parent());
+  if($target.hasClass('trigger')){
     $('.top').addClass('collapseUp');
     $('.bottom').addClass('collapseDown');
+    if($target.parent().parent().hasClass('fill')){
+      $target.parent().parent().removeClass('fill');
+      $('.top').removeClass('collapseUp');
+      $('.bottom').removeClass('collapseDown');
+      $('.top').addClass('expandDown');
+      $('.bottom').addClass('expandUp');
+      $('.middle').addClass('shrink');
+      setTimeout(function(){
+        $('.top').removeClass('expandDown');
+        $('.bottom').removeClass('expandUp');
+        $('.middle').removeClass('shrink');
+      }, 2000);
+    }else{
+      $target.parent().parent().addClass('fill');
+    }
   }else{
-    $(this).removeClass('rotateRight');
-    $(this).addClass('rotateLeft');
-    $('.top').removeClass('collapseUp');
-    $('.bottom').removeClass('collapseDown');
+    $('.fill').removeClass('fill');
+    if(!$target.parent().hasClass('rotateRight')){
+      $target.parent().removeClass('rotateLeft');
+      $target.parent().addClass('rotateRight');
+    }else{
+      $target.parent().removeClass('rotateRight');
+      $target.parent().addClass('rotateLeft');
+      setTimeout(function(){
+        $target.parent().removeClass('rotateLeft');
+      },900);
+      setTimeout(function(){
+        $('.top').removeClass('expandDown');
+        $('.bottom').removeClass('expandUp');
+      }, 2000);
+    }
   }
 });
 
